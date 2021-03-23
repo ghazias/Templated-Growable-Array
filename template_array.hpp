aa191ff1f2f4652be73c7581f640ee3b973f5788
Adrian G <adriansghazi@gmail.com>
2021-03-23 22:14:08 +0000

    Includes const at(), fixed invalid nullpointer assignment in pop_back()

--- template_array.hpp
+++ template_array.hpp
@@ -3,28 +3,33 @@
 
 #include <stdexcept>
 
+constexpr std::size_t DEFAULTCAP = 3;
+
 namespace dsc {
 template <typename T>
 class Array {
   public:
-    Array() : data_{new T[2]{}}, size_{0}, capacity_{2} {}
-	Array(std::size_t size) : data_{new T[size]{}}, size_{size}, capacity_{size} {} // constructor
+    Array() : data_{new T[DEFAULTCAP]{}}, size_{0}, capacity_{DEFAULTCAP} {} // empty constructor
+	Array(std::size_t size) : data_{new T[size]{}}, size_{0}, capacity_{size} {} // constructor
 	~Array() { delete[] data_; } // destructor
 	Array(const Array& original) : data_{new T[original.capacity()]{}}, size_{original.size()}, capacity_{original.capacity()} {
-	  	for (std::size_t i = 0; i < size(); ++i) {
-	  		data_[i] = original[i];
-	  	}
-	}
-	T& operator[](std::size_t i) { return data_[i]; }
+	  	for (std::size_t i = 0; i < size(); i++) { data_[i] = original[i]; }
+	} // copy constructor
+
+	Array(Array&& other) : data_{other.data()}, size_{other.size()}, capacity_{other.capacity()} {
+		other.data_ = nullptr;
+	} // move constructor
+
+	T& operator[](std::size_t i) { return data_[i]; } // index dereference operator
 	const T& operator[](std::size_t i) const { return data_[i]; }
 	bool operator==(const Array& other) const {
 	  if (size() != other.size()) { return false; }
-	  
-	  for(std::size_t i = 0; i < size(); i++) {
+
+	  for(std::size_t i = 0; i < size(); ++i) {
         if (data_[i] != other.data_[i]) { return false; }
       }
       return true;
-	}
+	} // equality operator
 	bool operator!=(const Array& other) const {
 		return !(*this == other);
 	}
@@ -32,26 +37,44 @@ class Array {
 		if (this == &other) {
 			return *this;
 		}
-		
+
 		size_ = other.size();
 		capacity_ = other.capacity_;
-		
+
 		delete[] data_;
 		data_ = new T[capacity()]{};
-		
+
 		for (std::size_t i = 0; i < size(); ++i) {
 			(*this)[i] = other[i];
 		}
-		
+
 		return *this;
 	} // copy assignment
+	
+	Array& operator=(Array&& other) {
+		if (this != other) {
+			delete[] data_;
+			data_ = other.data_;
+			other.data_ = nullptr;
+		}
+		
+		return *this;
+	} // move assignment
 	std::size_t size() const { return size_; }
 	std::size_t capacity() const { return capacity_; }
 	
 	T& at(std::size_t index) {
-		if (index > size()) {
+		if (index > size() || index < 0 || size() == 0) {
 			throw std::out_of_range("index out of array bounds");
 		} else {
+			return data_[index];
+		}
+	}
+	
+	const T& at(std::size_t index) const {
+		if (index > size() || index < 0 || size() == 0) {
+			throw std::out_of_range("index out of array bounds");
+		} else { 
 			return data_[index];
 		}
 	}
@@ -79,11 +102,7 @@ class Array {
 		data_ = resized_array;
 	}
 	T pop_back() {
-		if (size() == 0) {
-			return;
-		}
 		T result = (*this)[size() - 1];
-		(*this)[size() - 1] = nullptr;
 		--size_;
 		
 		return result;
